b94ea4da4be24d4520d0f42df1b61c6e6f23aa71
David Karam <david@posttool.com>
2014-10-23 13:01:22 -0700

    remove unused code, rename view function

--- app.js
+++ app.js
@@ -10,20 +10,15 @@ exports = module.exports = function(config, meta) {
   app.set('view engine', 'ejs');
   app.set('views',__dirname + '/views');
   app.use(express.static(__dirname + '/public'));
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
-//  app.use(express.urlencoded());
-//  app.use(express.json());
-//  app.use(express.compress());
 
   var Page = meta.model('Page');
-  //var News = meta.model('News');
 
   app.get('/favicon.ico', function(req, res, next){
     res.sendfile(__dirname + '/public/assets/favicon.ico');
   });
 
   app.get('/page', function (req, res, next) {
-    util.getSiteMapData(Page, false, th_page_view, function (err, site) {
+    util.getSiteMapData(Page, false, basic_page_view, function (err, site) {
       if (err) return next(err);
       res.json(site);
     });
@@ -48,12 +43,9 @@ exports = module.exports = function(config, meta) {
       Page.findOne({url: req.path}).populate("resources").exec(function (err, page) { //state: PUBLISHED
         if (!err && !page) return next(new Error("No such page " + req.path));
         if (err) return next(err);
-//        News.find({}, function (err, news) {
-//          if (err) return next(err);
           var next_page = util.getNextNode(util.findById(site, page.id));
           res.render('index', {site: site, page: page, resource_basepath: util.get_res_bp(config), next_page: next_page});
         });
-//      });
     });
   });
 
@@ -61,7 +53,7 @@ exports = module.exports = function(config, meta) {
 
 
 
-  function th_page_view(p) {
+  function basic_page_view(p) {
     return {
       id: p.id,
       title: p.title,
